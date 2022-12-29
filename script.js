f59ea3f2c17a49f38b09988338c16892f9e848b1
Mark Ramos <ramosmarkjoshua@gmail.com>
2022-12-29 09:17:27 +0800

    Responsive design and adding to do list app

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 //Sidebar click logic
-const sidebarItems = document.querySelectorAll('li');
+const sidebarItems = document.querySelectorAll('li')
 
 sidebarItems.forEach((item) =>
   item.addEventListener('click', function (e) {
     for (i of sidebarItems) {
-      i.classList.remove('clicked');
+      i.classList.remove('clicked')
     }
-    e.target.classList.add('clicked');
+    e.target.classList.add('clicked')
   })
-);
+)
+
+//hide side menu event listener
+const hiddenMenu = document.querySelector('.hiddenMenu')
+hiddenMenu.addEventListener('click', () => {
+  const sidebar = document.querySelector('#sidebar')
+  const body = document.querySelector('body')
+  sidebar.classList.toggle('hidden')
+  body.classList.toggle('body-class')
+  // body.setAttribute('id', 'body-id')
+})
