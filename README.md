7ae5a5a95c0e496f9a92be0cd592cb31dc33fb41
Mark Ramos <ramosmarkjoshua@gmail.com>
2023-01-07 21:42:50 +0800

    Added features and todolist link

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # Dashboard
 
-This is a homepage of a dashboard. I have included all my completed projects in here to act as my portfolio. It was created during my learning curve in TOP (The Odin Project). <br>
+This is a homepage of a dashboard. I have included all my completed projects in here to act as my temporary portfolio. It was created during my learning curve in TOP (The Odin Project). <br>
 
-Things I learned:<br>
+# Things I Learned
 
-- Deep understanding on when to use flex, grid, and table when layouting.<br>
-- How to create a responsive webpage using fr unit and grid<br>
-- Improved layout and color palette designing<br>
-- How to insert icons from fa and google fonts<br>
+- Deep understanding on when to use flex, grid, and table when layouting.
+- How to create a responsive webpage using fr unit and grid.
+- Improved layout and color palette designing.
+- How to insert icons from fa and google fonts.
 
 Live: https://learning-softdev.github.io/Dashboard/
 
-Future plans for this app:
+# Future Improvements
 
 - I, who have the admin access, will be able to easily add and store future projects in here just by clicking an add button. Currently I need to manually add it to the html file.
--- script.js
+++ script.js
@@ -1,21 +1,56 @@
 //Sidebar click logic
-const sidebarItems = document.querySelectorAll('li')
+const sidebarItems = document.querySelectorAll('.sidebar-contents li');
 
 sidebarItems.forEach((item) =>
   item.addEventListener('click', function (e) {
     for (i of sidebarItems) {
-      i.classList.remove('clicked')
+      i.classList.remove('clicked');
     }
-    e.target.classList.add('clicked')
+    e.target.closest('.sidebar-items').classList.add('clicked');
+
+    const sidebarText = e.target
+      .closest('.sidebar-items')
+      .querySelector('.text-content').textContent;
+    // Changing tab logic
+    if (sidebarText !== 'Home') {
+      document.querySelector('.main-container').classList.add('hidden');
+      document.querySelector('.container-trending-announcement').classList.add('hidden');
+
+      // If the msgContentDiv already exist, then don't create a new div, just edit the text.
+      if (document.querySelector('.msg-content') === null) {
+        const msgContent = document.createElement('div');
+        msgContent.classList.add('msg-content');
+        document
+          .querySelector('main')
+          .insertBefore(msgContent, document.querySelector('.main-container'));
+      }
+      if (document.querySelector('.msg-content') !== null) {
+        document.querySelector('.msg-content').textContent =
+          sidebarText + ' Page coming soon . . .';
+      }
+    } else {
+      document.querySelector('.main-container').classList.remove('hidden');
+      document.querySelector('.container-trending-announcement').classList.remove('hidden');
+
+      if (document.querySelector('.msg-content') !== null) {
+        document.querySelector('.msg-content').remove();
+      }
+    }
   })
-)
+);
 
 //hide side menu event listener
-const hiddenMenu = document.querySelector('.hiddenMenu')
+const hiddenMenu = document.querySelector('.hiddenMenu');
 hiddenMenu.addEventListener('click', () => {
-  const sidebar = document.querySelector('#sidebar')
-  const body = document.querySelector('body')
-  sidebar.classList.toggle('hidden')
-  body.classList.toggle('body-class')
-  // body.setAttribute('id', 'body-id')
-})
+  const sidebar = document.querySelector('#sidebar');
+  const body = document.querySelector('body');
+  sidebar.classList.toggle('hidden');
+  body.classList.toggle('body-class');
+});
+
+// profile icon
+const profileSelect = document.querySelector('#profile-select');
+const profileDropdown = document.querySelector('.dd_main');
+profileSelect.addEventListener('click', () => {
+  profileDropdown.classList.toggle('active');
+});
