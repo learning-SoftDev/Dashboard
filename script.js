03363a04848af6b557952233ffcd7e8330dffd8d
Mark Joshua Ramos <ramosmarkjoshua@gmail.com>
2023-02-27 22:23:33 +0800

    Edited page construction

--- script.js
+++ script.js
@@ -13,28 +13,36 @@ sidebarItems.forEach((item) =>
       .querySelector('.text-content').textContent;
     // Changing tab logic
     if (sidebarText !== 'Home') {
-      document.querySelector('.main-container').classList.add('hidden');
-      document.querySelector('.container-trending-announcement').classList.add('hidden');
+      // document.querySelector('.main-container').classList.add('hidden');
+      // document.querySelector('.container-trending-announcement').classList.add('hidden');
+      document.querySelector('main').classList.add('hidden');
+      document.querySelector('.msg-content').classList.remove('hidden');
 
-      // If the msgContentDiv already exist, then don't create a new div, just edit the text.
-      if (document.querySelector('.msg-content') === null) {
-        const msgContent = document.createElement('div');
-        msgContent.classList.add('msg-content');
-        document
-          .querySelector('main')
-          .insertBefore(msgContent, document.querySelector('.main-container'));
-      }
+      // // If the msgContentDiv already exist, then don't create a new div, just edit the text.
+      // if (document.querySelector('.msg-content-sub') === null) {
+      //   const msgContent = document.createElement('div');
+      //   const msgContentTitle = document.createElement('span');
+      //   const msgContentSubTitle = document.createElement('span');
+
+      //   msgContentTitle.classList.add('msg-content-title');
+      //   msgContentSubTitle.classList.add('msg-content-sub');
+      //   msgContent.classList.add('msg-content');
+
+      //   document.querySelector('body').insertBefore(msgContent, document.querySelector('main'));
+      //   msgContent.appendChild(msgContentTitle);
+      //   msgContent.appendChild(msgContentSubTitle);
+      // }
+
       if (document.querySelector('.msg-content') !== null) {
-        document.querySelector('.msg-content').textContent =
+        document.querySelector('.msg-content-title').textContent = 'Site is under construction.';
+        document.querySelector('.msg-content-sub').textContent =
           sidebarText + ' Page coming soon . . .';
       }
     } else {
-      document.querySelector('.main-container').classList.remove('hidden');
-      document.querySelector('.container-trending-announcement').classList.remove('hidden');
-
-      if (document.querySelector('.msg-content') !== null) {
-        document.querySelector('.msg-content').remove();
-      }
+      // document.querySelector('.main-container').classList.remove('hidden');
+      // document.querySelector('.container-trending-announcement').classList.remove('hidden');
+      document.querySelector('main').classList.remove('hidden');
+      document.querySelector('.msg-content').classList.add('hidden');
     }
   })
 );
